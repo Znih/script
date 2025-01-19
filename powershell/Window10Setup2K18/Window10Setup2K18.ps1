35198b49dc10ea6c89c75edd70cf5beb4d0dbb95
Znih <dr.marco.buschmann@proton.me>
2025-01-19 21:12:32 +0100

    Update Window10Setup2K18.ps1

--- powershell/Window10Setup2K18/Window10Setup2K18.ps1
+++ powershell/Window10Setup2K18/Window10Setup2K18.ps1
@@ -1,21 +1,3 @@
-# Windows 10 1703 Privacy Setup 2018
-Inhalt
-* [Powershell](https://github.com/hinzigers/Debloat-Windows-10)
-* [Binary](#)
-* [Password](http://hacker:matrix2021@matrixhacker.de/20171231_Komplexe-Passwoerter_KeePass2.pdf)
-* [Generator](#)
-* [Quellcode](https://github.com/Artikel-278-VV/script/blob/master/powershell/2018_Windows10-Setup.ps1)
-
-# Testdatei für Windows 10 Version 1703
-Nur für IT Professionals
-* 2018_Windows10-Uninstall.exe
-* 2018_Windows10-Setup.exe (rc1 for Activists / not for Windowsdomain)
-* TrustIPs.txt (Good IPs)
-* UnTrustIPs.txt
-
-
-
-
 #=============================================================================
 # HELP:           https://technet.microsoft.com/en-us/library/hh849827.aspx
 # FILE:           2018_Windows10-Setup.ps1
